014b188aa8fee61d885821983957b4041724b20c
Mihael Ištvanović <mistvanov@gmail.com>
2019-04-23 19:18:46 +0200

    Done task #2

--- db/connect.js
+++ db/connect.js
@@ -1,10 +1,10 @@
 const { Pool } = require('pg');
 
 const pool = new Pool({
-  user: '', //marinokol
-  host: '', //138.68.87.73
-  database: '', // db_marinokol
-  password: '', // your password
+  user: 'mihaelis', //marinokol
+  host: '138.68.87.73', //138.68.87.73
+  database: 'db_mihaelis', // db_marinokol
+  password: 'db_mihaelis', // your password
   port: 5432, // 5432
 })
 
